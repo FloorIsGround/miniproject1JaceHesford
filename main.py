d7293c5ab8d1a046d16477241f8aba74674c9076
FloorIsGround <jrhesford@mail.fhsu.edu>
2026-02-10 23:00:58 -0600

    Got closing price data and plotted graphs

--- main.py
+++ main.py
@@ -8,14 +8,19 @@
 # (5/5 points) Proper import of packages used.
 # (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
 # (10/10 points) Store this information in a list that you will convert to a array in NumPy.
+
 # (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
+
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
 
 import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
 import pprint
+import copy
 
 mystocks = ['SNDK', 'BX', 'FTNT', 'GOOG', 'V']
 stockdata = {}
@@ -23,5 +28,19 @@ stockdata = {}
 for stock in mystocks:
     dat = yf.Ticker(stock)
     last10 = dat.history(period="10d")
-    stockdata[stock] = last10['Close']
-pprint.pprint(stockdata)
+    stockdata[stock] = []
+
+    # Creating a list of the last 10 days closing prices for each stock ticker
+    for price in last10['Close']:
+        stockdata[stock].append(price)
+    mystock = np.array(stockdata[stock])
+
+    # HL for finding high and low values in the time period.
+    hl = copy.copy(stockdata[stock])
+    hl.sort()
+
+    plt.plot(mystock)
+    plt.axis((0, 10, hl[0] - 10, hl[-1] + 10))
+    plt.xlabel('Trading Days Ago')
+    plt.ylabel('Closing Price')
+    plt.show()
